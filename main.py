2a5aa266a959a7fd4db8b29f1a1efcc6df577a87
hcpassos <higorcpassos@gmail.com>
2021-05-10 23:10:33 -0300

    tratamento e group by

--- main.py
+++ main.py
@@ -32,14 +32,37 @@ def texto_para_lista(elemento, delimitador='|'):
     """
     return elemento.split(delimitador)
 
+
+def trata_datas(elemento):
+    """
+    Recebe um dicionário e cria um novo campo ANO-MES
+    Retorna o mesmo dicionário com um novo campo
+    """
+    elemento['ano_mes'] = '-'.join(elemento['data_iniSE'].split('-')[:2])
+    return elemento
+
+
+def chave_uf(elemento):
+    """
+    Receber um dicionário
+    Retornar uma tupla (UF,dict)
+    """
+    chave = elemento['uf']
+    return (chave, elemento)
+
+
 # pcollection
 dengue = (
-    pipeline | "Leitura do dataset de dengue" >>
-    ReadFromText('casos_dengue.txt', skip_header_lines=1) |
-    "De texto para lista" >>
-    beam.Map(texto_para_lista) | "De lista para dicionário" >>
-    beam.Map(lista_para_dicionario, colunas_dengue) | "Mostrar resultado" >>
-    beam.Map(print)
+    pipeline
+    | "Leitura do dataset de dengue" >>
+    ReadFromText('casos_dengue.txt', skip_header_lines=1)
+    | "De texto para lista" >> beam.Map(texto_para_lista)
+    | "De lista para dicionário" >>
+    beam.Map(lista_para_dicionario, colunas_dengue)
+    | "Criar campo ano-mes" >> beam.Map(trata_datas)
+    | "Criar chave pelo estado" >> beam.Map(chave_uf)
+    | "Agrupar por UF" >> beam.GroupByKey()
+    | "print" >> beam.Map(print)
 )
 
 pipeline.run()
