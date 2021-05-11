8accde79ab6a9228399a8d389e3157856472a994
hcpassos <higorcpassos@gmail.com>
2021-05-10 21:19:47 -0300

    leitura do csv

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-import apache_beam
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.options.pipeline_options import PipelineOptions
 
-print('Apache Beam importado!')
-print(dir(apache_beam))
+pipeline_options = PipelineOptions(arg=None)
+pipeline = beam.Pipeline(options=pipeline_options)
+
+def texto_para_lista(elemento, delimitador='|'):
+    """
+    Recebe um texto e um delimitador 
+    Retorna uma lista de elementos pelo delimitador
+    """
+    return elemento.split(delimitador)
+
+#pcollection
+dengue = (
+    pipeline
+    | "Leitura do dataset de dengue" >> 
+        ReadFromText('casos_dengue.txt',skip_header_lines=1)
+    | "De texto para lista" >> beam.Map(texto_para_lista)
+    | "Mostrar resultador" >> beam.Map(print)
+)
+
+pipeline.run()
